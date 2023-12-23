2b30c7d8e11e0bd4c387d3353ed75c1ef38d89eb
chakerhenitn <chakerhenitn@gmail.com>
2023-12-23 10:36:39 +0400

    headless

--- src/components/ProductCards.tsx
+++ src/components/ProductCards.tsx
@@ -11,14 +11,16 @@ const ProductCard = ({ product }: IProps) => {
   const { description, imageURL, price, title, category } = product;
   //End use of destructring
   return (
-    <div className=" max-w-sm md:max-w-lg mx-auto md:mx-0 border rounded-md p-2 flex flex-col">
+    <div className=" max-w-sm md:max-w-lg mx-auto md:mx-0 border rounded-md p-2 flex flex-col space-y-3">
       <Image
         imageURL={imageURL}
         alt={"Product Name"}
-        className="  rounded-md  h-52 w-full lg:object-cover"
+        className="  rounded-md h-52 w-full lg:object-cover"
       />
-      <h3>{title}</h3>
-      <p>{txtSlicer(description)}</p>
+      <h3 className="text-lg font-semibold">{txtSlicer(title, 25)}</h3>
+      <p className="text-xs text-gray-500 break-words">
+        {txtSlicer(description)}
+      </p>
       <div className="flex items-center my-4 space-x-2">
         <span className="w-5 h-5 bg-indigo-600 rounded-full cursor-pointer" />
         <span className="w-5 h-5 bg-yellow-600 rounded-full cursor-pointer" />
@@ -26,15 +28,14 @@ const ProductCard = ({ product }: IProps) => {
       </div>
 
       <div className="flex items-center justify-between">
-        <span>{price}</span>
-
+        <span className="text-lg text-indigo-600 font-semibold">${price}</span>
         <Image
-          alt={category.name}
           imageURL={category.imageURL}
+          alt={category.name}
           className="w-10 h-10 rounded-full object-bottom"
         />
       </div>
-      <div className="flex items-center justify-between space-x-2 mt-5">
+      <div className="flex items-center justify-between space-x-2">
         <Button className="bg-red-700">DELETE</Button>
         <Button className="bg-green-700">EDIT</Button>
       </div>
